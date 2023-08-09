650b1a9e7d822cdd9131b07132a710ffa5dc6e0d
Junaid Jamel <109211380+JunaidJameel@users.noreply.github.com>
2023-08-09 12:28:05 +0500

    Update searchScreen_controller..dart

--- lib/controller/searchScreen_controller..dart
+++ lib/controller/searchScreen_controller..dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SearchController extends GetxController {
-  var searchTxtFieldController = TextEditingController();
+  
   var initialFoodSelected = 0.obs;
   var initialCategorySelected = 0.obs;
 
